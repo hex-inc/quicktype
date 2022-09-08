cd30e7f5e28df8276e9451dee2a5a47865a5c9c6
Tim Nonet <tnonet@hex.tech>
2022-09-08 14:14:38 -0500

    Only build quicktype-core (#3)

--- script/publish.sh
+++ script/publish.sh
@@ -10,5 +10,5 @@ npm run build
 # npm publish --ignore-scripts # Don't rebuild
 
 ( cd build/quicktype-core ; node build.js publish )
-( cd build/quicktype-typescript-input ; node build.js publish )
-( cd build/quicktype-graphql-input ; node build.js publish )
+# ( cd build/quicktype-typescript-input ; node build.js publish )
+# ( cd build/quicktype-graphql-input ; node build.js publish )
